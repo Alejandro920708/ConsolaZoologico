0dd3ef0d51ff38e61e21abcc733ea876059e8bfb
rpriego <alex-10priegoalejandro@hotmail.com>
2022-01-06 11:44:44 -0600

    modificacion de master

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsolaZoologico
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Rey");
         }
     }
 }
